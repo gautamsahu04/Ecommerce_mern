3a321dbe82f542aa746a8fdcabe973ae13f8a158
gautamsahu04 <sahu9450076454@gmail.com>
2025-03-26 01:36:34 +0530

    Added MongoDB error logging

--- server/dB_Connection/db_connection.js
+++ server/dB_Connection/db_connection.js
@@ -1,16 +1,15 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
+const database = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error, "error connecting to database");
+  }
+};
 
- 
-const database= async()=>{
-    try {
-        const connect = await mongoose.connect(process.env.MONGODB_URL)
-        console.log("MongoDB Connected")
-     } catch (error) {
-        console.log(error, 'error connecting to database')
-        
-     }
-
-}
-
-export default database
+export default database;
